adb244460937e79f95290344ac298e4b333825f5
alexdelorenzo <you@example.com>
2020-09-24 14:52:14 -0400

    Works

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,65 @@
-use std::io::{self, BufRead};
+use std::io::{self, Read, BufRead, Result as IoResult};
 use std::collections::HashSet;
 use std::iter::{Iterator};
 use std::str::FromStr;
 // use rayon::prelude
 
+use byte_string::{ByteString};
 use clap::ArgMatches;
 use clap::clap_app;
 
 const LOWEST: usize = 0;
 const LINES: &str = "LINES";
 const REVERSE: &str = "reverse";
+const NEW_LINE: u8 = 10;
+const CARRIAGE_RETURN: u8 = 13;
+
 
 type LineNums = Vec<usize>;
-type NthStr = (usize, String);
+type ByteLine = ByteString;
+type ByteLineResult = IoResult<ByteLine>;
+type NthStr = (usize, ByteLine);
+
 // type NthStrIterable = Box<dyn Iterator<Item = NthStr>>;
+
+#[derive(Debug, Copy, Clone)]
+pub struct ByteLines<B> {
+    buf: B,
+}
+
+impl<B: Iterator<Item = IoResult<u8>>> Iterator for ByteLines<B> {
+    type Item = ByteLineResult;
+
+    fn next(&mut self) -> Option<ByteLineResult> {
+        let mut buf = vec![];
+        let bytes = &mut self.buf;
+
+        while let Some(Ok(byte)) = bytes.next() {
+          buf.push(byte);
+
+          if is_newline(byte) {
+            break;
+          }
+        }
+
+        let byte_str = ByteString::new(buf);
+        Some(Ok(byte_str))
+    }
+}
+
+trait ReadByteLines<T> {
+  fn byte_lines(self: Self) -> ByteLines<T>;
+}
+
+impl<T> ReadByteLines<T> for T {
+  fn byte_lines(self: T) -> ByteLines<T> {
+    ByteLines { buf: self }
+  }
+}
+
+fn is_newline(chr: u8) -> bool {
+  chr == NEW_LINE || chr == CARRIAGE_RETURN
+}
 
 
 fn get_matches_from_cli<'a>() -> ArgMatches<'a> {
@@ -34,8 +80,8 @@ pub fn run() {
     let mut line_nums = get_line_nums(&matches);
 
     let stdin = io::stdin();
-    let nth_lines = stdin.lock()
-        .lines()
+    let nth_lines = stdin.lock().bytes()
+        .byte_lines()
         .map(|line| line.unwrap())
         .enumerate();
 
@@ -47,49 +93,49 @@ pub fn run() {
 }
 
 fn to_str(num: &str) -> usize {
-    FromStr::from_str(num).unwrap()
+  FromStr::from_str(num).unwrap()
 }
 
 fn get_line_nums(matches: &ArgMatches) -> LineNums {
-    let mut line_nums = 
-        if let Some(lines) = matches.values_of(LINES) {
-            lines.map(to_str).collect() 
-        } else {
-            vec![]
-        };
+  let mut line_nums = 
+    if let Some(lines) = matches.values_of(LINES) {
+      lines.map(to_str).collect() 
+    } else {
+      vec![]
+    };
 
-    line_nums.sort();
-    line_nums
+  line_nums.sort();
+  line_nums
 }
 
 fn include_lines<T: IntoIterator<Item = NthStr>>(
-    lines: &mut LineNums, 
-    content: T
+  lines: &mut LineNums, 
+  content: T
 ) {
-    for (nth, line) in content {
-        if nth == lines[LOWEST] {
-            println!("{}", line);
-            lines.remove(LOWEST);
-        }
+  for (nth, line) in content {
+    if nth == lines[LOWEST] {
+      println!("{:?}", line);
+      lines.remove(LOWEST);
+    }
 
-        if lines.is_empty() {
-            break
-        }
-    } 
+    if lines.is_empty() {
+      break
+    }
+  } 
 }
 
 fn exclude_lines<T: Iterator<Item = NthStr>>(
-    lines: &LineNums, 
+    lines: &LineNums,
     content: T
 ) {
     let mut lines: HashSet<&usize> = 
       lines.iter().collect();
-    
+
     for (nth, line) in content {
         if lines.contains(&nth) {
             lines.remove(&nth);
         } else {
-            println!("{}", line);
+            println!("{:?}", line);
         }
     }
 }
